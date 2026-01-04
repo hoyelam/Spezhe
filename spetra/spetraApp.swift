59ddca31a6f66e03fc8f3680e17cf7f44d6e6652
Hoye Lam <hoyelam@kin-yee.com>
2026-01-04 12:41:49 +0100

    Initial Commit

---
+++ spetra/spetraApp.swift
@@ -0,0 +1,17 @@
+//
+//  spetraApp.swift
+//  spetra
+//
+//  Created by Hoye Lam on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct spetraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
